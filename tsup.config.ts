d99231a6e87471aff15cd4c5f250c2ccb232fbf3
Shahrad Elahi <shahradq@gmail.com>
2023-12-03 21:12:43 +0330

    support `esm` exports

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/index.tsx'],
+  format: ['esm', 'cjs'],
+  sourcemap: true,
+  target: 'esnext',
+  outDir: 'dist'
+});
